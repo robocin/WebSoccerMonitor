1aea0b753ac715aeb8686f2d1a9afce3e1aaa9aa
mfbs <mateusfbsoares@gmail.com>
2021-09-25 15:46:39 -0300

    feat-backend/add isKicking to the stats_log_of each player and use it to flash the player outline white when it kicks

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,16 @@
 import flask
-from flask import Flask
+from flask import Flask, make_response
 import os
+from flask.json import jsonify
 import pandas as pd
 import json
+from flask_cors import CORS, cross_origin
 
 TMP_DF = "./log.rcg"
 QUANTITY_OF_PLAYERS_PER_TEAM = 11
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
@@ -17,6 +20,7 @@ def hello_world():
 def convert_rcg_to_csv():
     # recieve a *.rcg.gz file as an http request
     file = flask.request.files["file"]
+
     if file.filename == '':
         return "file is empty"
     elif not (file.filename.endswith(".rcg") or file.filename.endswith(".rcg.gz")):
@@ -35,10 +39,14 @@ def convert_rcg_to_csv():
     df = pd.read_csv(f)
     os.system("rm log.rcg")
 
-    # create dictionary that will be send back as a json string
-    df = df.head(n=500) # TODO: delete this line!
+    # create and populate dictionary that will be send back as a json string
+    # df = df.head(n=100) # TODO: delete this line!
+    time_range = len(df["show_time"])
     final_json = {
         "match":{
+            # "quantity_of_frames": time_range, // this should be the line. however, 
+            "quantity_of_frames": 5999,
+            "game_state_log": [],
             "team_l_name": str(df["team_name_l"][0]),
             "team_r_name": str(df["team_name_r"][0]),
             "team_l_score": [],
@@ -50,27 +58,32 @@ def convert_rcg_to_csv():
         "players": []
     }
 
-
-    # populate dictionary with data
-    time_range = len(df["show_time"])
-
     for i in range(0, time_range):
         final_json["ball"]["stats_log"].append({"x": df["ball_x"][i], "y": df["ball_y"][i]})
         final_json["match"]["team_l_score"].append(int(df["team_score_l"][i]))
         final_json["match"]["team_r_score"].append(int(df["team_score_r"][i]))
+        final_json["match"]["game_state_log"].append(str(df["playmode"][i]))
 
     for side in ["l","r"]:
         for i in range(0,QUANTITY_OF_PLAYERS_PER_TEAM):
-            final_json["players"].append({ "id":i + (0 if side=="l" else 11), "side": side, "name": f"player_{side}{i+1}", "stats_log":[]}) 
+            final_json["players"].append({ "id":i + (0 if side=="l" else 11), "side": side, "name": f"player_{side}{i+1}", "stats_log":[], "other_stats":[]}) 
+
             for time in range(0, time_range):
-                final_json["players"][i + (0 if side=="l" else 11)] ["stats_log"].append({
+                aux_kicking_count = 0
+                is_kicking_in_this_frame = int(df[f"player_{side}{i+1}_counting_kick"][time]) > aux_kicking_count
+                final_json["players"][i + (0 if side=="l" else 11)]["stats_log"].append({
                     "x": float(df[f"player_{side}{i+1}_x"][time]), # x position of the palyer at the current time
                     "y": float(df[f"player_{side}{i+1}_y"][time]), # y position of the palyer at the current time
                     "bodyAngle": float(df[f"player_{side}{i+1}_body"][time]), # angle of the body at the current time
                     "neckAngle": float(df[f"player_{side}{i+1}_neck"][time]), # angle of the neck at the current time
                     "viewWidth": float(df[f"player_{side}{i+1}_view_width"][time]), # width of the player view at the current time
-                    "countingKick": int(df[f"player_{side}{i+1}_counting_kick"][time]) # quantity of kicks of this player at the current time
+                    "countingKick": int(df[f"player_{side}{i+1}_counting_kick"][time]), # quantity of kicks of this player at the current time
+                    "isKicking":1 if is_kicking_in_this_frame else 0 # quantity of kicks of this player at the current time
                     })
+                if is_kicking_in_this_frame:
+                    aux_kicking_count += 1 
+
+
 
 
     os.system("rm log.rcg.csv")
@@ -78,4 +91,4 @@ def convert_rcg_to_csv():
     return json.dumps(final_json)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5001")
+    app.run(host="0.0.0.0", port="5002")
